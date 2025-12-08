a1f244887c55217af94974becb087fc22b40e114
Truls Henriksson <trulshenriksson@gmail.com>
2025-12-08 14:28:51 +0100

    Added Lorenz gif to the readme

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Purpose
 
-This is a project designed to help visualize solutions to ODEs, by giving simple interfaces to get numerical solutions, as well as plotting and animating those.
+This is a project designed to help visualize solutions to ODEs, by giving simple interfaces to get numerical solutions, as well as plotting and animating them.
+
+![Lorenz attractor in 3D](/assets/Lorenz%20attractor%20rotating.gif)
+
--- examples/lorenz_attractor.py
+++ examples/lorenz_attractor.py
@@ -7,7 +7,7 @@ from ODEplotter import ODE, SolutionAnimator
 plt.rc("axes3d", mouserotationstyle="azel")
 
 
-class LorenzAttractor:
+class LorenzAttractor(ODE):
     def __init__(self, rho=28.0, sigma=10.0, beta=8/3):
         self.fixed_points = np.array([
             [np.sqrt(beta * (rho - 1)), np.sqrt(beta * (rho - 1)), rho - 1],
@@ -17,19 +17,25 @@ class LorenzAttractor:
         self.sigma = sigma
         self.beta = beta
 
-    def derivative(self, t, state):
-        x, y, z = state
-        dx = self.sigma * (y - x)
-        dy = x * (self.rho - z) - y
-        dz = x * y - self.beta * z
-        return np.array((dx, dy, dz))
+        def derivative(t, state):
+            x, y, z = state
+            dx = sigma * (y - x)
+            dy = x * (rho - z) - y
+            dz = x * y - beta * z
+            return np.array((dx, dy, dz))
+        
+        super().__init__(derivative)
 
     def get_lines_and_initial_conditions(self, ax, num_points_per_fixed_point, variance=20.0, **plot_kwargs):
         lines = ax.plot(*([] for _ in range(3 * 2 * num_points_per_fixed_point)), marker=".", markevery=[-1], **plot_kwargs)
+        
+        oranges = plt.get_cmap("autumn")
+        blues = plt.get_cmap("winter")
         for line in lines[0::2]:
-            line.set_color("blue")
+            line.set_color(oranges(np.random.random() * 0.6))
         for line in lines[1::2]:
-            line.set_color("orange")
+            line.set_color(blues(np.random.random() * 0.4 + 0.2))
+        
         # Generate points around [0, 0, 0]
         points = np.random.multivariate_normal(np.zeros(3), np.identity(3) * variance, 2 * num_points_per_fixed_point)
         # Move them to be centered on the fixed points
@@ -43,7 +49,6 @@ class LorenzAttractor:
 
 # Problem definition
 lorenz = LorenzAttractor()
-ode = ODE(lorenz.derivative)
 
 # Figure and Axes
 fig = plt.figure(figsize=(6, 6))
@@ -59,7 +64,7 @@ lines, initial_conditions = lorenz.get_lines_and_initial_conditions(ax, number_o
 # Animation
 animator = SolutionAnimator()
 for line, y0 in zip(lines, initial_conditions):
-    sol = ode.solve(t0, y0, "RK4", 0.01)
+    sol = lorenz.solve(t0, y0, "RK4", 0.01, use_jit=True)
     animator.animate(
         sol, SolutionAnimator.phase_diagram_updater_3d(line, trail_length=200), SolutionAnimator.line_resetter_3d(line)
     )
