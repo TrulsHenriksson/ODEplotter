a9edbd76e1bba4538d018e033120c48aabff098b
Truls Henriksson <trulshenriksson@gmail.com>
2026-05-24 23:39:48 +0200

    Fixed weighted_sum to be jited again

--- ODEplotter/utils/weighted_sum.py
+++ ODEplotter/utils/weighted_sum.py
@@ -1,6 +1,9 @@
+from numba import jit
+
 from .types import VectorArray, WeightArray, Vector
 
 
+@jit
 def weighted_sum(vectors: VectorArray, weights: WeightArray) -> Vector:
-    """Dot product along the first axis of `vectors`."""
-    return vectors.T.dot(weights).T
+    """Dot product along the first axis of `vectors`, equivalent to `vectors.T.dot(weights).T`."""
+    return (vectors.T * weights).sum(axis=-1).T
