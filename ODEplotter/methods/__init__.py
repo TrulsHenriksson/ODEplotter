9c6bbbe28adc510d33f61549f003bb7d0bc200b4
Truls Henriksson <trulshenriksson@gmail.com>
2026-06-01 21:53:18 +0200

    Removed predictors.py, reused AB weights

--- ODEplotter/methods/__init__.py
+++ ODEplotter/methods/__init__.py
@@ -21,29 +21,29 @@ __all__ = ["METHODS"]
 
 EULER = EulersMethod()
 
-AB1 = AdamsBashforth([1.0])
-AB2 = AdamsBashforth([3/2, -1/2])
-AB3 = AdamsBashforth([23/12, -16/12, 5/12])
-AB4 = AdamsBashforth([55/24, -59/24, 37/24, -9/24])
-AB5 = AdamsBashforth([1901/720, -2774/720, 2616/720, -1274/720, 251/720])
+AB1 = AdamsBashforth(AB1_WEIGHTS)
+AB2 = AdamsBashforth(AB2_WEIGHTS)
+AB3 = AdamsBashforth(AB3_WEIGHTS)
+AB4 = AdamsBashforth(AB4_WEIGHTS)
+AB5 = AdamsBashforth(AB5_WEIGHTS)
 
 # Implicit linear multistep methods
 
 IMPLICIT_EULER = ImplicitEulersMethod()
 
 TRAPEZOIDAL_RULE = TrapezoidalRule()
-AM0 = AdamsMoulton([1.0], predictor="AB1")
-AM1 = AdamsMoulton([0.5, 0.5], predictor="AB2")
-AM2 = AdamsMoulton([5/12, 8/12, -1/12], predictor="AB3")
-AM3 = AdamsMoulton([9/24, 19/24, -5/24, 1/24], predictor="AB4")
-AM4 = AdamsMoulton([251/720, 646/720, -264/720, 106/720, -19/720], predictor="AB5")
+AM0 = AdamsMoulton(AM0_WEIGHTS, predictor_weights=AB1_WEIGHTS)
+AM1 = AdamsMoulton(AM1_WEIGHTS, predictor_weights=AB2_WEIGHTS)
+AM2 = AdamsMoulton(AM2_WEIGHTS, predictor_weights=AB3_WEIGHTS)
+AM3 = AdamsMoulton(AM3_WEIGHTS, predictor_weights=AB4_WEIGHTS)
+AM4 = AdamsMoulton(AM4_WEIGHTS, predictor_weights=AB5_WEIGHTS)
 
-BDF1 = BackwardDifferentialFormula([1.0], 1.0)
-BDF2 = BackwardDifferentialFormula([4/3, -1/3], 2/3)
-BDF3 = BackwardDifferentialFormula([18/11, -9/11, 2/11], 6/11)
-BDF4 = BackwardDifferentialFormula([48/25, -36/25, 16/25, -3/25], 12/25)
-BDF5 = BackwardDifferentialFormula([300/137, -300/137, 200/137, -75/137, 12/137], 60/137)
-BDF6 = BackwardDifferentialFormula([360/147, -450/147, 400/147, -225/147, 72/147, -10/147], 60/147)
+BDF1 = BackwardDifferentialFormula(BDF1_Y_WEIGHTS, BDF1_DERIVATIVE_WEIGHT)
+BDF2 = BackwardDifferentialFormula(BDF2_Y_WEIGHTS, BDF2_DERIVATIVE_WEIGHT)
+BDF3 = BackwardDifferentialFormula(BDF3_Y_WEIGHTS, BDF3_DERIVATIVE_WEIGHT)
+BDF4 = BackwardDifferentialFormula(BDF4_Y_WEIGHTS, BDF4_DERIVATIVE_WEIGHT)
+BDF5 = BackwardDifferentialFormula(BDF5_Y_WEIGHTS, BDF5_DERIVATIVE_WEIGHT)
+BDF6 = BackwardDifferentialFormula(BDF6_Y_WEIGHTS, BDF6_DERIVATIVE_WEIGHT)
 
 # Fixed-step Runge-Kutta methods
 
@@ -118,7 +118,8 @@ METHODS: dict[str, SolutionMethod] = {
 }
 
 ALIASES: dict[str, list[str]] = {
-    "euler": ["explicit_euler"],
+    "euler": ["explicit_euler", "forward_euler"],
+    "implicit_euler": ["backward_euler"],
     "heun": ["rkh2"],
     "ralston": ["rkr2"],
     "trapezoidal_rule": ["trapezoidal", "trapezoid"],
--- ODEplotter/methods/linear_multistep/adams_moulton.py
+++ ODEplotter/methods/linear_multistep/adams_moulton.py
@@ -1,8 +1,7 @@
 import numpy as np
 
-from typing import Callable, Generator
+from typing import Generator
 from ...utils.types import *
-from ...utils.predictors import Predictors
 from ...utils.root_finder import vector_newton
 
 from ..solution_method import SolutionMethod, weighted_sum
@@ -15,8 +14,7 @@ def adams_moulton(
     y: Vector,
     h: Time,
     weights: WeightArray,
-    predictor: Callable[[Time, Vector, VectorArray], Vector],
-    corrector: Callable[[Callable[[Vector], Vector], Vector], Vector],
+    predictor_weights: WeightArray,
 ) -> Generator[SolutionPoint]:
     first_weight = weights[0]
     # Start with the previous derivatives all equal to the initial diff
@@ -32,8 +30,8 @@ def adams_moulton(
             return next_y - y - h * (first_weight * first_derivative + previous_derivatives_average)
 
         # Find the next y value that makes the deficit function zero
-        next_y_guess = predictor(h, y, derivatives)
-        y = corrector(deficit, next_y_guess)
+        next_y_guess = y + h * weighted_sum(derivatives, predictor_weights)
+        y = vector_newton(deficit, next_y_guess)
 
         # Move the old derivatives back one step and calculate the new one
         derivatives[1:] = derivatives[:-1]
@@ -45,23 +43,23 @@ class AdamsMoulton(SolutionMethod):
     compiled_method = None
 
     weights: WeightArray
-    predictor: Callable[[Time, Vector, VectorArray], Vector]  # (h, y, derivatives) -> next_y_guess
+    predictor_weights: WeightArray
 
-    def __init__(self, weights, predictor: str):
+    def __init__(self, weights, predictor_weights):
         """Method which uses a weighted sum of several previous and the next `f` values to approximate `y(t+h)`."""
         super().__init__()
         self._weights = weights
-        self._predictor = predictor
+        self._predictor_weights = predictor_weights
 
     def _validate(self):
         if self.validated:
             return
 
         self.weights = to_weight_array(self._weights)
-        self.predictor = Predictors.methods[self._predictor]
+        self.predictor_weights = to_weight_array(self._predictor_weights)
 
-        del self._weights, self._predictor
+        del self._weights, self._predictor_weights
         self.validated = True
 
     def _prepare_arguments(self, derivative: DerivativeFunction, t0: Time, y0: Vector, h: Time, use_jit: bool):
-        return (derivative, t0, y0, h, self.weights, self.predictor, vector_newton)
+        return (derivative, t0, y0, h, self.weights, self.predictor_weights)
